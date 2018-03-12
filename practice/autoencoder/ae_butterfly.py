47a1764abaac67eb4b5e97900bba2bc892f7c21e
Margaret Manalo <mdymanalo@gmail.com>
2018-03-12 19:26:33 +0900

    autoencoder: implemented encoding and decoding parts

--- practice/autoencoder/ae_butterfly.py
+++ practice/autoencoder/ae_butterfly.py
@@ -2,10 +2,15 @@ import sys
 sys.path.insert(0, '../../tools/keras')
 from DatasetGenerator import DatasetGenerator
 from ImageTransform import ImageTransform
+from keras.layers import Dense, Input
+from keras.models import Model
+import matplotlib.pyplot as plt
+import numpy as np
 
 pathData = '../data/leedsbutterfly/images'
-pathDataset = '../data/dataset.txt'
-dataSize = 86
+pathTrain = '../data/dataset_train.txt'
+pathTest = '../data/dataset_test.txt'
+dataSize = 10
 imgWidth = 300
 imgHeight = 300
 
@@ -14,8 +19,52 @@ transformList = []
 transformList.append(ImageTransform(0, [imgWidth, imgHeight]))
 transformList.append(ImageTransform(10))
 
-dataset = DatasetGenerator(pathData, pathDataset, transformList, imgWidth, imgHeight, dataSize, 'true')
-print(dataset.getSize())
-(x_train, x_label) = dataset.generate()
-print(x_train)
+datasetTrain = DatasetGenerator(pathData, pathTrain, transformList, imgWidth, imgHeight, dataSize, 'false')
+datasetTest = DatasetGenerator(pathData, pathTest, transformList, imgWidth, imgHeight, dataSize, 'false')
 
+(x_train, _) = datasetTrain.generateBatch(datasetTrain.generateIndexList())
+(x_test, _) = datasetTest.generateBatch(datasetTest.generateIndexList())
+print(x_train.shape)
+
+encoding_dim = 10000
+input_img = Input(shape=(270000,))
+
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+decoded = Dense(270000, activation='sigmoid')(encoded)
+autoencoder = Model(input_img, decoded)
+encoder = Model(input_img, encoded)
+encoded_input = Input(shape=(encoding_dim,))
+decoder_layer = autoencoder.layers[-1]
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+print(len(x_train))
+print(np.prod(x_train.shape[1:3]))
+# x_train = np.asarray(x_train)
+# x_test = np.asarray(x_test)
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=10, shuffle=True, validation_data=(x_test, x_test))
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
